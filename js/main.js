228783771f63fffdc9e22878c27e7ebe1f925f23
Ritesh <riteshkokam@gmail.com>
2020-09-29 09:29:15 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    const responsive = {
+        0: {
+            items: 1
+        },
+        320: {
+            items: 1
+        },
+        620: {
+            items: 2
+        },
+        920: {
+            items: 3
+        }
+    }
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+    // click to scroll top
+    $('.move-up span').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000)
+    })
+    
+    // AOS Instance
+    AOS.init();
+  });
